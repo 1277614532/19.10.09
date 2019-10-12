04aba3f9dc94fbe199ee6613ccdb7724756e2893
Christopher <50388315+1277614532@users.noreply.github.com>
2019-10-12 14:53:23 +0800

    Update controller.java

--- controller.java
+++ controller.java
@@ -1,1 +1,18 @@
+@Controller
+@RequestMapping("players")   //自定义的名字
+public class PlayersController {
+
+	@Resource(name="playersService")
+	private PlayersService playersService;
+	
+	@RequestMapping("/test.do")
+	public ModelAndView test() {
+		ModelAndView modelandview = new ModelAndView();
+		List<PlayersModel> list = playersService.queryAll();
+		
+		System.out.println(list);
+		modelandview.addObject(list);
+		return modelandview;
+	}
+}
 
